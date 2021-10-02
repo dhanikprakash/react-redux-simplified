773843e838d5de7a7937dce53869fe18c2a31691
dhanikprakash <dhanikprakash@gmail.com>
2021-10-02 23:42:58 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { createStore } from 'redux'
+
+const counterReducer = (state =0, action) => {
+  console.log(`current state ${state} action ${action.type}`);
+  switch(action.type){
+    case 'INCREMENT':
+       return state + 1;
+    case 'DECREMENT':
+      return state -1;
+    case 'RESET': 
+       return 0;
+       default:
+         return state;
+  }
+}
+const store = createStore(counterReducer);
 
 function App() {
+  console.log('Rendering...');
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      </div>
+          <button onClick={ e => store.dispatch({type: 'INCREMENT'})} >Plus</button>
+          <button onClick={ e => store.dispatch({type: 'DECREMENT'})} >minues</button>
+          <button onClick={ e => store.dispatch({type: 'RESET'})} >Reset</button>
     </div>
-  );
+    </>
+  )
 }
+
+store.subscribe(() => console.log(store.getState()));
 
 export default App;
